59942d294f5c47d0e121aa27bdad3a542ea93cb5
dhightnm <dhightnm@gmail.com>
2021-07-07 18:47:29 -0700

    Added api pull

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(morgan("short"));
+
 app.use('/api', require('./routes/openSkyRouter'));
-
-
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
--- routes/openSkyRouter.js
+++ routes/openSkyRouter.js
@@ -1,13 +1,20 @@
 const router = require('express').Router();
 const axios = require('axios');
+require('dotenv').config();
 
 router.get('/area/:latmin/:lonmin/:latmax/:lonmax', async (req, res) => {
     const latmin = req.params.latmin;
     const lonmin = req.params.lonmin;
     const latmax = req.params.latmax;
     const lonmax = req.params.lonmax;
-    const areaRes = await axios.get(`https://opensky-network.org/api/states/all?lamin=${latmin}&lomin=${lonmin}&lamax=${latmax}&lomax=${lonmax}`);
-    console.log(latmin, latmax, lonmin, lonmax)
+
+    try {
+    const areaRes = await axios.get(`https://${process.env.OPENSKY_USER}:${process.env.OPENSKY_PASS}@opensky-network.org/api/states/all?lamin=${latmin}&lomin=${lonmin}&lamax=${latmax}&lomax=${lonmax}`);
+    console.log(areaRes.data);
+    }
+    catch(err) {
+        console.log(err);
+    }
 });
 
 module.exports = router;
