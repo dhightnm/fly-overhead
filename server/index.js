237b0fc48681b287763771a584456310c5260146
dhightnm <dhightnm@gmail.com>
2021-07-09 16:21:27 -0700

    Polish

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -9,5 +10,15 @@ app.use(express.json());
 app.use(morgan("short"));
 
 app.use('/api', require('./routes/openSkyRouter'));
+
+
+// use for server side rendering later
+
+// app.use(express.static(path.join(__dirname, "..", "build")));
+// app.use(express.static("public"));
+
+// app.use((req, res, next) => {
+//     res.sendFile(path.join(__dirname, "..", "client", "public/index.html"));
+//   });
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
