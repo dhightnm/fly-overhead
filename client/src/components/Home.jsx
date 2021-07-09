3a4407b63701b108c745f1cd4a974feddff8f996
dhightnm <dhightnm@gmail.com>
2021-07-08 21:02:13 -0700

    Added plane state, retrieving data based on map bounds

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,30 +1,45 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { MapContainer, Marker, Popup, TileLayer, useMapEvents} from 'react-leaflet';
+import axios from 'axios';
 
-function MyComponent() {
-  const map = useMapEvents({
-    click: () => {
-      map.locate()
-    },
-    locationfound: (location) => {
-      console.log('location found:', location)
-    },
-    zoomend: (e) => {
-      console.log(e);
-    },
-  })
-  return null
-}
+
 
 const Home = () => {
 
-    const position = [55, -115.1398]
+  const [planes, setPlanes] = useState([]);
+
+  function MyComponent() {
+    const map = useMapEvents({
+      click: () => {
+        map.locate()
+      },
+      locationfound: (location) => {
+        console.log('location found:', location)
+      },
+      moveend: async () => {
+        const bounds = map.getBounds();
+        const wrapBounds = map.wrapLatLngBounds(bounds);
+        // console.log(bounds._southWest.lat, bounds._southWest.lng, bounds._northEast.lat, bounds._northEast.lng);
+        console.log(wrapBounds);
+        const res = await axios.get(`https://opensky-network.org/api/states/all?lamin=${wrapBounds._southWest.lat}&lomin=${wrapBounds._southWest.lng}&lamax=${wrapBounds._northEast.lat}&lomax=${wrapBounds._northEast.lng}`);
+        console.log(res.data);
+        setPlanes(res.data.states)
+        
+      },
+    })
+    return null
+  }
+
+  const mapRef = useRef(null)
+
+    const position = [46.79, -351.70]
     return <>
     <MapContainer
       center={position} 
       zoom={8} 
       scrollWheelZoom={true}
-      style={{height: 500, width: 900}}>
+      style={{height: 500}}
+      ref={mapRef}>
         <MyComponent />
     <TileLayer
       attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
