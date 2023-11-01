a606d4e60010e308446767b7a346d346782348ac
Devin Hight <devin@deliverr.com>
2023-11-01 02:55:06 -0600

    better plane markers

--- client/src/components/PlaneMarker.jsx
+++ client/src/components/PlaneMarker.jsx
@@ -4,24 +4,26 @@ import L from 'leaflet';
 import customPlaneIcon from '../assets/plane.png';
 import 'leaflet-rotatedmarker';
 
-const PlaneMarker = ({ plane }) => {
+import './planeMarker.css'; // Import a stylesheet for your component
 
-    const customIcon = L.icon({
-        iconUrl: customPlaneIcon,
-        iconSize: [32, 32],
-        iconAnchor: [16, 16],
-        popupAnchor: [0, 0],
-    });
+const CUSTOM_ICON = L.icon({
+    iconUrl: customPlaneIcon,
+    iconSize: [32, 32],
+    iconAnchor: [16, 16],
+    popupAnchor: [0, 0],
+});
 
-    const rotationAngle = plane.true_track;
-
+const PlaneMarker = ({ plane: { latitude, longitude, callsign, icao24, baro_altitude, velocity, true_track } }) => {
     return (
-        <Marker position={[plane.latitude, plane.longitude]} icon={customIcon} rotationAngle={rotationAngle}>
+        <Marker position={[latitude, longitude]} icon={CUSTOM_ICON} rotationAngle={true_track}>
             <Popup>
-                {plane.callsign} <br />
-                Altitude: {Math.round(plane.baro_altitude * 3.281)}ft <br />
-                Speed: {Math.round(plane.velocity * 1.944)}kts <br />
-                Heading: {Math.round(plane.true_track)}
+                <div className="plane-popup">
+                    <div><strong>Callsign:</strong> <span>{callsign}</span></div>
+                    <div><strong>ICAO24:</strong> <span>{icao24}</span></div>
+                    <div><strong>Altitude:</strong> <span>{Math.round(baro_altitude * 3.281)}ft</span></div>
+                    <div><strong>Speed:</strong> <span>{Math.round(velocity * 1.944)}kts</span></div>
+                    <div><strong>Heading:</strong> <span>{Math.round(true_track)}</span></div>
+                </div>
             </Popup>
         </Marker>
     );
