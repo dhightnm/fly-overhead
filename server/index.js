48d2551cfff0b94ba558a6fe085b95bdf1f79d40
Devin Hight <devin@deliverr.com>
2023-11-01 02:43:09 -0600

    working dynamo connection HC

--- server/index.js
+++ server/index.js
@@ -17,10 +17,10 @@ app.use('/api', require('./routes/openSkyRouter'));
 
 // populateDatabase();
 // populateDatabaseDynamo();
-updateDatabaseFromAPIDynamo();
-setInterval(updateDatabaseFromAPIDynamo, 60000);
+// updateDatabaseFromAPIDynamo();
+// setInterval(updateDatabaseFromAPIDynamo, 60000);
 // setInterval(updateDatabaseFromAPI, 300000);
 // deleteStaleRecordsDynamo();
-setInterval(deleteStaleRecordsDynamo, 2 * 60 * 60 * 1000);
+// setInterval(deleteStaleRecordsDynamo, 2 * 60 * 60 * 1000);
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
