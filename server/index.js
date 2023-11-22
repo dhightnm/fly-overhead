7f8e43ae98973651353353cffab14f55aad387c4
Devin Hight <devin@deliverr.com>
2023-11-22 01:45:52 -0700

    add cors

--- server/index.js
+++ server/index.js
@@ -13,6 +13,8 @@ const {
 const PORT = process.env.PORT || 3001;
 // const HTTPS_PORT = process.env.HTTPS_PORT || 3001;
 
+const allowedOrigins = ['http://flyoverhead.com', 'http://www.flyoverhead.com', 'http://localhost:3000'];
+
 // const privateKey = fs.readFileSync(process.env.SSL_KEY_PATH, 'utf8');
 // const certificate = fs.readFileSync(process.env.SSL_CERT_PATH, 'utf8');
 // const ca = process.env.SSL_CA_PATH ? fs.readFileSync(process.env.SSL_CA_PATH, 'utf8') : null;
@@ -23,9 +25,15 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:3000',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
+  origin(origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not '
+                + 'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
 }));
 app.use(express.json());
 app.use(morgan('short'));
