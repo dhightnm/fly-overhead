4a389e0bfb8bdf628da74c529b52de5732936a95
dhightnm <dhightnm@gmail.com>
2025-01-09 02:16:30 -0700

    Implement fullscreen toggle for map and enhance styling; update satellite marker icon

--- client/src/components/SatMarker.jsx
+++ client/src/components/SatMarker.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import { Marker, Popup } from 'react-leaflet';
+import customSatIcon from '../assets/sat.png';
+
+const CUSTOM_ICON = L.icon({
+    iconUrl: customSatIcon,
+    iconSize: [32, 32],
+    iconAnchor: [16, 16],
+    popupAnchor: [0, 0],
+});
+
 
 const SatMarker = ({ sat }) => {
 
     return (
-        <Marker position={[sat.satlat, sat.satlng, sat.satalt]}>
+        <Marker position={[sat.satlat, sat.satlng, sat.satalt]} icon={CUSTOM_ICON}>
             <Popup>
             {sat.satname} <br />
             {sat.satid} <br />
